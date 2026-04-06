c1f8645265e7da898e97ceb6e2c22e0e746b1c04
glasseshead <glasseshead.tetra@gmail.com>
2026-04-05 22:30:55 -0700

    brought doinker and goalrush into tasks after realizing i forgot they existed

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -4,6 +4,8 @@
 #include "ladybrown.hpp"
 #include "climb.hpp"
 #include "color.hpp"
+#include "goalrush.hpp"
+#include "doinker.hpp"
 #include "tasks.hpp"
 
 void initTasks() {
@@ -11,5 +13,7 @@ void initTasks() {
     pros::Task mogoClampTask(runMogoClamp, "mogo clamp task");
     pros::Task ladybrownTask(runLadybrown, "ladybrown task");
     pros::Task climbTask(runClimb, "climb task");
+    pros::Task goalRushTask(runGoalRush, "goal rush task");
+    pros::Task doinkerTask(runDoinker, "doinker task");
     pros::Task checkColorTask(checkColor, "check color task");
 }
